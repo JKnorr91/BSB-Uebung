b5d65d29ac6ee83c3efbc7bd03998f62421ca71f
Andreas Lang <andiwg@arcor.de>
2016-11-09 03:20:16 +0100

    useless files

--- oostups/dep/panic.d
+++ oostups/dep/panic.d
@@ -1,1 +1,3 @@
-build/panic.o: device/panic.cc
+build/panic.o: device/panic.cc device/panic.h guard/gate.h \
+ device/cgastr.h object/strbuf.h object/o_stream.h machine/cgascr.h \
+ machine/io_port.h machine/cpu.h
