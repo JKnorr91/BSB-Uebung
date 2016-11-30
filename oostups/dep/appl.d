c3953e0b22ff4d3f51519790919d625517bf8a10
Andreas Lang <andiwg@arcor.de>
2016-11-30 17:47:30 +0100

    deps

--- oostups/dep/appl.d
+++ oostups/dep/appl.d
@@ -1,4 +1,4 @@
-build/appl.o: user/appl.cc user/appl.h device/cgastr.h object/strbuf.h \
- object/o_stream.h machine/cgascr.h machine/io_port.h machine/cpu.h \
- guard/secure.h guard/guard.h guard/gate.h object/chain.h guard/locker.h \
- object/queue.h
+build/appl.o: user/appl.cc user/appl.h thread/coroutine.h machine/toc.h \
+ device/cgastr.h object/strbuf.h object/o_stream.h machine/cgascr.h \
+ machine/io_port.h machine/cpu.h guard/secure.h guard/guard.h \
+ guard/gate.h object/chain.h guard/locker.h object/queue.h
