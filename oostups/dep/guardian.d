c39970097431235bddafd5651ab26986b9921514
Andreas Lang <andiwg@arcor.de>
2016-12-20 16:17:35 +0100

    Aufgabe 5

--- oostups/dep/guardian.d
+++ oostups/dep/guardian.d
@@ -2,4 +2,4 @@ build/guardian.o: guard/guardian.cc device/cgastr.h object/strbuf.h \
  object/o_stream.h machine/cgascr.h machine/io_port.h machine/plugbox.h \
  guard/gate.h machine/cpu.h object/chain.h device/panic.h \
  device/keyboard.h machine/keyctrl.h machine/key.h machine/pic.h \
- guard/guard.h guard/locker.h object/queue.h
+ guard/guard.h guard/locker.h user/debug.h object/queue.h
--- oostups/dep/keyboard.d
+++ oostups/dep/keyboard.d
@@ -2,4 +2,5 @@ build/keyboard.o: device/keyboard.cc device/keyboard.h machine/keyctrl.h \
  machine/io_port.h machine/key.h guard/gate.h device/cgastr.h \
  object/strbuf.h object/o_stream.h machine/cgascr.h machine/cpu.h \
  object/chain.h machine/plugbox.h machine/pic.h user/appl.h \
- thread/coroutine.h machine/toc.h
+ thread/entrant.h thread/coroutine.h machine/toc.h thread/scheduler.h \
+ thread/dispatch.h object/queue.h
