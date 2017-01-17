f4480e09cdf5c932e27f167dee9214bafb6e4e5a
JKnorr <jakobknorr@gmail.com>
2017-01-17 17:12:00 +0100

    implemented guarded_organizer
    and guarded_keyboard

--- oostups/syscall/guarded_keyboard.cc
+++ oostups/syscall/guarded_keyboard.cc
@@ -8,4 +8,12 @@
 /* Systemaufrufschnittstelle zur Tastatur.                                   */
 /*****************************************************************************/
 
-/* Hier muesst ihr selbst Code vervollstaendigen */ 
+#include "syscall/guarded_keyboard.h"
+#include "guard/secure.h"
+#include "device/keyboard.h"
+
+Key Guarded_Keyboard::getkey() {
+    { Secure section;
+        //return Keyboard::getkey();
+    }
+}
--- oostups/syscall/guarded_organizer.cc
+++ oostups/syscall/guarded_organizer.cc
@@ -10,20 +10,29 @@
 
 #include "syscall/guarded_organizer.h"
 #include "thread/organizer.h"
+#include "guard/secure.h"
 
 
 void Guarded_Organizer::ready(Thread& that) {
-
+    { Secure section;
+        Scheduler::ready(that);
+    }
 }
 
 void Guarded_Organizer::kill(Thread& that) {
-
+    { Secure section;
+        Organizer::kill(that);
+    }
 }
 
 void Guarded_Organizer::exit() {
-
+    { Secure section;
+        Scheduler::exit();
+    }
 }
 
 void Guarded_Organizer::resume() {
-
+    { Secure section;
+        Scheduler::resume();
+    }
 }
